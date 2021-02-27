a11e235be22ee8ca4e13a4dc084a6bac9eee407c
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-02-27 19:57:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import "./App.css";
+
+import Home from "./pages/home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/single-room" component={SingleRoom} />
+    </>
   );
 }
 
